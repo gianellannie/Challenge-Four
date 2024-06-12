994a073187fe3bbf3300bf6bb38a27192e5cbedd
Gianella Annie <gianella.annie.060@gmail.com>
2024-06-11 20:57:43 -0500

    Añadir archivos

---
+++ src/main/java/forohub/Challenge_Four/ChallengeFourApplication.java
@@ -0,0 +1,13 @@
+package forohub.Challenge_Four;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeFourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/forohub/Challenge_Four/ChallengeFourApplicationTests.java
@@ -0,0 +1,13 @@
+package forohub.Challenge_Four;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
