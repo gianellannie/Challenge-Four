958917f2f430a7f8a7b9c372c6caa603e191f8b1
Gianella Annie <gianella.annie.060@gmail.com>
2024-06-12 18:55:24 -0500

    Añadir entidades

---
+++ src/main/java/forohub/Challenge_Four/dominio/curso/Curso.java
@@ -0,0 +1,20 @@
+package forohub.Challenge_Four.dominio.curso;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cursos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+}
---
+++ src/main/java/forohub/Challenge_Four/dominio/usuario/Usuario.java
@@ -0,0 +1,22 @@
+package forohub.Challenge_Four.dominio.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String contrasena;
+}
