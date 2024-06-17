51f9a3c99ec1ee61ed5ab4b903436cef6f7df140
Gianella Annie <gianella.annie.060@gmail.com>
2024-06-16 20:26:12 -0500

    Actualizar autenticación

--- src/main/java/forohub/Challenge_Four/dominio/usuario/UsuarioRepositorio.java
+++ src/main/java/forohub/Challenge_Four/dominio/usuario/UsuarioRepositorio.java
@@ -1,6 +1,8 @@
 package forohub.Challenge_Four.dominio.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepositorio extends JpaRepository<Usuario,Long> {
+    UserDetails findByNombre(String nombreUsuario);
 }
